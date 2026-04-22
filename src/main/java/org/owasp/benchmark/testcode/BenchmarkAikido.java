6de8a3429f8046226015806d50785817d0feeecf
thibautn-lab <thibaut.nijs@acagroup.be>
2026-04-22 16:59:19 +0200

    Update BenchmarkAikido.java

--- src/main/java/org/owasp/benchmark/testcode/BenchmarkAikido.java
+++ src/main/java/org/owasp/benchmark/testcode/BenchmarkAikido.java
@@ -5,15 +5,13 @@ import java.sql.Statement;
 
 public class VulnerabilityTest {
 
-    // 1. Hardcoded Secret (Credential Leak)
     private static final String DB_PASSWORD = "super_secret_password_12345!";
 
     public void unsafeDatabaseQuery(String userInput) {
         try {
             Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "admin", DB_PASSWORD);
             
-            // 2. SQL Injection (The "Classic" Vulnerability)
-            // Using a string concatenation instead of a PreparedStatement
+            String CamelCaseTest = "Test";
             Statement statement = conn.createStatement();
             String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
             ResultSet rs = statement.executeQuery(query);

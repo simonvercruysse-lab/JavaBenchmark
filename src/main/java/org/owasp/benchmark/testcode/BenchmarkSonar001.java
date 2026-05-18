d71af0f21217a7740dec2e75df44c80e0586d481
thibautn-lab <thibaut.nijs@acagroup.be>
2026-05-18 13:09:53 +0200

    Create BenchmarkSonar001.java

---
+++ src/main/java/org/owasp/benchmark/testcode/BenchmarkSonar001.java
@@ -0,0 +1,26 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class VulnerabilityTest {
+
+    private static final String DB_PASSWORD = "super_secret_password_12345!";
+
+    public void unsafeDatabaseQuery(String userInput) {
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/db", "admin", DB_PASSWORD);
+            
+            String CamelCaseTest = "Test";
+            Statement statement = conn.createStatement();
+            String query = "SELECT * FROM users WHERE username = '" + userInput + "'";
+            ResultSet rs = statement.executeQuery(query);
+
+            while (rs.next()) {
+                System.out.println("User found: " + rs.getString("username"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
